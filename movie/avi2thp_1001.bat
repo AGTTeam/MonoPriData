81598bb6fd8f2b144845234645e9407946ba3f22
Illidan <illidanrulez@gmail.com>
2021-09-26 09:17:36 +0200

    Fixed credits apostrophe.

--- movie/avi2thp_1001.bat
+++ movie/avi2thp_1001.bat
@@ -1,4 +1,4 @@
-ffmpeg -i movie1001.avi -vf fps=29970/1000 -q:v 1 frames_1001_done/movie1001_%%04d.png
+ffmpeg -i movie1001.avi -vf fps=29970/1000 -q:v 1 frames_1001_done/movie1001_%%05d.png
 @echo off
 for %%f in (frames_1001_done\*.png) do (
     magick convert -define colorspace:auto-grayscale=false -colorspace sRGB -type TrueColor -strip -interlace none -alpha remove -flatten -quality 88 -sampling-factor 4:2:0 %%f %%~df%%~pf%%~nf.jpg
